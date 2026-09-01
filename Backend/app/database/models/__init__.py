2585fb8fe82e3182936d7a45f577e86007d906b3
Meriem Hamri <meriemhamri78@gmail.com>
2026-09-01 02:22:04 +0100

    8

--- Backend/app/database/models/__init__.py
+++ Backend/app/database/models/__init__.py
@@ -1,3 +1,9 @@
 from app.database.models.document import DocumentModel
+from app.database.models.conversation import ConversationModel
+from app.database.models.message import MessageModel
 
-__all__ = ["DocumentModel"]
+__all__ = [
+    "DocumentModel",
+    "ConversationModel",
+    "MessageModel",
+]
